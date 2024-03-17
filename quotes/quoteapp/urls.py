568581e24babd675843322816d580f09684c2194
ivan1melnyk <melnyk.ivan20a@gmail.com>
2024-03-17 16:59:33 +0200

    parser

--- quotes/quoteapp/urls.py
+++ quotes/quoteapp/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('tag/', views.tag, name='tag'),
     path('<int:pk>', views.AuthorView.as_view(), name='author'),
     path('add_author/', views.add_author, name='add_author'),
+    path('tag/<int:tag_id>/', views.quotes_by_tag, name='quotes_by_tag')
 ]
--- quotes/quoteapp/views.py
+++ quotes/quoteapp/views.py
@@ -3,6 +3,7 @@ from .forms import TagForm, QuoteForm, AuthorForm
 from .models import Tag, Author, Quote
 from django.contrib.auth.decorators import login_required
 from django.views.generic import DetailView
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -11,13 +12,24 @@ def main(request):
     quotes = Quote.objects.all()
     authors = Author.objects.all()
     tags = Tag.objects.all()
-    return render(request, 'quoteapp/index.html', {"quotes": quotes, "authors": authors, "tags": tags})
+    paginator = Paginator(quotes, 5)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'quoteapp/index.html', {"page_obj": page_obj, "authors": authors, "tags": tags})
 
 
 class AuthorView(DetailView):
     model = Author
     template_name = 'quoteapp/author.html'
     context_object_name = 'author'
+
+
+def quotes_by_tag(request, tag_id):
+    tag = get_object_or_404(Tag, pk=tag_id)
+    quotes = tag.quote_set.all()
+    return render(request, 'quoteapp/quotes_by_tag.html', {'tag': tag, 'quotes': quotes})
 
 
 @login_required
